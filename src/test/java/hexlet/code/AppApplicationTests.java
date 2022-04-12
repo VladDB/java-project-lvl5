6891572d1d7cacbcc2abd9bea4351452a5b02a39
MacBook <VladDB@github.com>
2022-04-12 20:49:02 +0300

    after linter

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -2,9 +2,9 @@ package hexlet.code;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.github.database.rider.core.api.configuration.DBUnit;
-import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.junit5.api.DBRider;
+//import com.github.database.rider.core.api.configuration.DBUnit;
+//import com.github.database.rider.core.api.dataset.DataSet;
+//import com.github.database.rider.junit5.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -50,10 +50,10 @@ class AppApplicationTests {
 	void testCreateUsers() throws Exception {
 		MockHttpServletResponse responsePost = mockMvc
 				.perform(post("/api/users").contentType(MediaType.APPLICATION_JSON)
-						.content("{\"firstName\": \"Jackson\", " +
-								"\"lastName\": \"Bind\", " +
-								"\"email\": \"jl@mail.ru\", " +
-								"\"password\": \"password\"}"))
+						.content("{\"firstName\": \"Jackson\", "
+								+ "\"lastName\": \"Bind\", "
+								+ "\"email\": \"jl@mail.ru\", "
+								+ "\"password\": \"password\"}"))
 				.andReturn().getResponse();
 
 		assertThat(responsePost.getStatus()).isEqualTo(200);
