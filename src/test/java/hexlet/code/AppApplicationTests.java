8261451e6cb3796a986448dd5a828f8214a120af
MacBook <VladDB@github.com>
2022-04-16 17:00:40 +0300

    add frontend

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -2,9 +2,7 @@ package hexlet.code;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//import com.github.database.rider.core.api.configuration.DBUnit;
-//import com.github.database.rider.core.api.dataset.DataSet;
-//import com.github.database.rider.junit5.api.DBRider;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @AutoConfigureMockMvc
 @Transactional
 //@DBRider
+//@DBUnit(alwaysCleanBefore = true)
 //@DataSet("users.yml")
 class AppApplicationTests {
 
