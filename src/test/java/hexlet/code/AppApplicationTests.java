b594fc9ddbca5e9ccaca0fdf8be1f2909009a784
MacBook <VladDB@github.com>
2022-05-04 13:12:55 +0300

    fix linter

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -6,10 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 class AppApplicationTests {
-
-	@Test
-	void contextLoads() {
-		assertThat(true).isTrue();
-	}
-
+   @Test
+   void contextLoads() {
+	   assertThat(true).isTrue();
+   }
 }
--- src/test/java/hexlet/code/controller/TaskTest.java
+++ src/test/java/hexlet/code/controller/TaskTest.java
@@ -24,7 +24,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
